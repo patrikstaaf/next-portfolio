296805d77f7e5ec0b9fbeedb31bbfc7a6e19fb52
patrikstaaf <staaf.patrik@gmail.com>
2022-10-10 12:55:53 +0200

    Webhhok console.log working fine, now try to add validation and if so revalidate the page

--- web/pages/api/revalidate.js
+++ web/pages/api/revalidate.js
@@ -1,10 +1,4 @@
-// import { isValidSignature, SIGNATURE_HEADER_NAME } from '@sanity/webhook';
-
-// const secret = process.env.SANITY_WEBHOOK_SECRET;
-
-// import { isValidRequest } from '@sanity/webhook';
-
-const secret = process.env.SANITY_WEBHOOK_SECRET;
+import { isValidSignature, SIGNATURE_HEADER_NAME } from '@sanity/webhook';
 
 export default async function handler(req, res) {
   // if (req.method !== 'POST') {
@@ -17,9 +11,23 @@ export default async function handler(req, res) {
   //   return;
   // }
 
-  console.log(req.body);
-  console.log('slug', req.body.slug);
-  console.log('type', req.body.type);
+  try {
+    const signature = req.headers[SIGNATURE_HEADER_NAME].toString();
+    if (
+      !isValidSignature(
+        JSON.stringify(req.body),
+        signature,
+        process.env.SANITY_WEBHOOK_SECRET
+      )
+    )
+      return res.status(401).json({ msg: 'Invalid request' });
+
+    const { slug } = req.body;
+    await res.revalidate(`/projects/${slug}`);
+    await res.revalidate(`/projects`);
+  } catch (error) {
+    res.status(500).json({ err: error });
+  }
 
   // try {
   //   const {
